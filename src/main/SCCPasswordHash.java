eaed6af469bf234839114cc203a993d2f21e420e
Kai Mindermann <kai.mindi@gmx.de>
2020-07-19 11:24:45 +0200

    updates documentation of SCCPasswordHash

--- src/main/SCCPasswordHash.java
+++ src/main/SCCPasswordHash.java
@@ -6,10 +6,13 @@ import COSE.CoseException;
 import COSE.PasswordHashMessage;
 
 /**
- * Class representing the Hash resulting from executing PasswordHashing.
- * SCCPasswordHash includes the byte[] representation of a COSE message. The
- * byte[] contains the hashed plaintext as well as all the parameters used
- * during hashing.
+ * Container for a Cryptographic Password Hash.
+ * 
+ * SCCHash contains a byte[] representation of a COSE message. The byte[]
+ * contains the password hash as well as all the parameters used during hashing.
+ * The inclusion of the used parameters in the password hash ensures that
+ * validation implementation code does not need to know the used algorithm or
+ * parameters before validation, but can parse it from the COSE message.
  * 
  * @author Lisa
  *
@@ -19,8 +22,8 @@ public class SCCPasswordHash extends AbstractSCCPasswordHash {
 	private SecureCryptoConfig scc = new SecureCryptoConfig();
 
 	/**
-	 * Constructor that gets the byte[] representation of the COSE message resulting
-	 * from PasswordHashing
+	 * Constructor that creates a new SCCPasswordHash object based on existing COSE
+	 * message (password hash) bytes.
 	 * 
 	 * @param hashMsg: byte[] of COSE message
 	 */
